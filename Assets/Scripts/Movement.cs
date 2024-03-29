76670253fb771b0a0a52e11388688a40d815ec1b
Huseyin Caliskan <huseyin@weave.nl>
2024-03-29 15:59:24 +0100

    movement script & huseyin scene

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+    private Rigidbody _rb;
+    void Update()
+    {
+        
+    }
+}
