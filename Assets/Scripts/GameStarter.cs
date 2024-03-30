fbfc6c23efdc6a41d6a419c8ce8a8ab95988afad
Stormlolwut <stormwitzier123@gmail.com>
2024-03-30 20:27:39 +0100

    - Much more new stuff!

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(PlayerInputManager))]
 public class GameStarter : MonoBehaviour
@@ -8,6 +9,7 @@ public class GameStarter : MonoBehaviour
     private bool _isGameStarted;
 
     [SerializeField] private GameObject areaPrefab;
+    [SerializeField] private GameObject playerWorldCanvas;
     
     private void Awake  ()
     {
@@ -19,10 +21,27 @@ public class GameStarter : MonoBehaviour
         
         var area = Instantiate(areaPrefab, Vector3.zero, Quaternion.identity);
         var enemySpawner = area.GetComponentInChildren<EnemySpawner.EnemySpawner>();
-
+        
         enemySpawner.playerCamera = player.camera;
         enemySpawner.playerTransform = player.transform;
         
+        LinkHealthBarWithPlayer(player, area);
+        
         _isGameStarted = true;
+    }
+
+    private void LinkHealthBarWithPlayer(PlayerInput player, GameObject area)
+    {
+        var canvasInstance = Instantiate(playerWorldCanvas, Vector3.zero, Quaternion.identity);
+        
+        // Assign the player's camera to the player's canvas so we can see the canvas
+        var parentHealthBar = canvasInstance.GetComponentInChildren<Image>();
+
+        //Give the player a reference to the image
+        var playerHealth = player.GetComponentInChildren<PlayerHealth>();
+        playerHealth.SetHealthBar(parentHealthBar.gameObject);
+        
+        //Set the camera to the right canvas
+        canvasInstance.GetComponent<Canvas>().worldCamera = player.camera;
     }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,39 +1,43 @@
-﻿using System;
-using Interfaces;
+﻿using Interfaces;
 using UnityEngine;
 
 public class Health : MonoBehaviour, IDamagable
 {
-    [SerializeField] private GameObject healthBar;
+    [SerializeField] protected float maxHealth = 100f;
+    [SerializeField] protected float currentHealth = 100f;
     
-    [SerializeField] private float maxHealth = 100f;
-    [SerializeField] private float currentHealth = 100f;
-    
-    [SerializeField] private bool isGodMode;
-    
+    [SerializeField] protected bool isGodMode;
     
     public delegate void DieAction(GameObject deadObject);
     public event DieAction OnDie;
+    
+    public delegate void DamageAction(float damage);
+    public event DamageAction OnDamage;
 
     public float CurrentHealth => currentHealth;
-    
+
     public float Damage(float damage)
     {
-        if (isGodMode) return currentHealth;
+        if (isGodMode || damage <= 0) return maxHealth;
         
         currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
 
-        if (currentHealth > 0) return currentHealth;
+        OnDamage?.Invoke(damage);
         
-        OnDie?.Invoke(gameObject);
+        if (!(currentHealth <= 0)) return currentHealth;
+        
         SetToDeath();
-        
-        return 0;
-    }
 
+        return currentHealth;
+    }
+    
     private void SetToDeath()
     {
+        OnDie?.Invoke(gameObject);
         gameObject.SetActive(false);
         currentHealth = maxHealth;
     }
+    
+
 }
